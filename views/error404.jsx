489a0dbf9defef241d56f6ddb5004e13a073898b
Jordan Fudge <jordanfudge95@gmail.com>
2023-05-04 09:33:53 -0400

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,15 @@ function error404() {
       <main>
         <h1>404: PAGE NOT FOUND</h1>
         <p>Oops, sorry, we can't find this page!</p>
+        <div>
+          <img src="/images/sad-puppy.jpg" alt="Sad Puppy" />
+          <div>
+            {" "}
+            Photo by
+            <a href="https://unsplash.com/@bacila_vlad">Bacila Vlad</a>
+            on <a href="https://unsplash.com/">Unsplash</a>
+          </div>
+        </div>
       </main>
     </Def>
   );
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,16 @@ function home() {
     <Def>
       <main>
         <h1>HOME</h1>
+        <div>
+          <img src="/images/pancake.jpg" alt="Stack of Pancakes" />
+          <div>
+            Photo by
+            <a href="https://unsplash.com/@khlebnikovayulia">
+              Yulia Khlebnikova
+            </a>
+            on <a href="https://unsplash.com/">Unsplash</a>
+          </div>
+        </div>
         <a href="/places">
           <button className="btn-primary">Places Page</button>
         </a>
