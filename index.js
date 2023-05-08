6afc811719ffeced0866e697b20aae57ccca31c9
Jordan Fudge <jordanfudge95@gmail.com>
2023-05-08 14:06:35 -0400

    Delete functionality implemented

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Modules and Globals
 const express = require("express");
 require("dotenv").config();
+const methodOverride = require("method-override");
 const app = express();
 
 // Express Settings
-app.use(express.urlencoded({ extended: true }));
+app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
-app.use("/places", require("./controllers/places"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 //Controllers & Routes
 app.use("/places", require("./controllers/places"));
